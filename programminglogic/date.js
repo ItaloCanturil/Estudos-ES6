09059b42d50d7a2fd91a793cb8a7a954cfef9efb
Canturilsnaik <italo.cantur@gmail.com>
2021-01-29 08:25:48 -0300

    Learn Date inside the Javascript

--- programminglogic/date.js
+++ programminglogic/date.js
@@ -3,25 +3,27 @@
 // Dois paramêtros são ano e mês os outros podem ser omitidos, que são, dia, hora, minuto, segundo e milesimos.
 // Pode ser mandado uma string 
 const data = new Date('2019-01-20 20:17:00')
-console.log('Dia', data.getDate());
-console.log('Mês', data.getMonth()); // Mês começa do zero
-console.log('Ano', data.getFullYear());
-console.log('Hora', data.getHours());
-console.log('Min', data.getMinutes());
-console.log('Seg', data.getSeconds());
-console.log('ms', data.getMilliseconds());
-console.log('Dia semana', data.getDay()); // 0 é domingo, 6 é sabado
-console.log(data.toString());
+// console.log('Dia', data.getDate());
+// console.log('Mês', data.getMonth()); // Mês começa do zero
+// console.log('Ano', data.getFullYear());
+// console.log('Hora', data.getHours());
+// console.log('Min', data.getMinutes());
+// console.log('Seg', data.getSeconds());
+// console.log('ms', data.getMilliseconds());
+// console.log('Dia semana', data.getDay()); // 0 é domingo, 6 é sabado
+// console.log(data.toString());
+
+formataData(data)
 
 function formataData(data) {
   const dia = zeroAEsquerda(data.getDate());
   const mes = zeroAEsquerda(data.getMonth() + 1);
   const ano = zeroAEsquerda(data.getFullYear());
   const hora = zeroAEsquerda(data.getHours());
-  const seg = zeroAEsquerda(data.getMinutes());
+  const min = zeroAEsquerda(data.getMinutes());
   const segundos = zeroAEsquerda(data.getSeconds());
 
-  return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`
+  return console.log(`${dia}/${mes}/${ano} ${hora}:${min}:${segundos}`)
 }
 
 function zeroAEsquerda (num) {
