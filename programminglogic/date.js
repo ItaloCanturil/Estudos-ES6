e1996220b9b011f60ff21c4baad09adc7a13ca24
Canturilsnaik <italo.cantur@gmail.com>
2021-01-29 08:20:21 -0300

    Learn date inside the Javascript

---
+++ programminglogic/date.js
@@ -0,0 +1,29 @@
+// Date é contado em milesimos de segundos a partir do dia 01/01/1970
+// se tiver só um paramêtro no construtor Date, é milisegundos.
+// Dois paramêtros são ano e mês os outros podem ser omitidos, que são, dia, hora, minuto, segundo e milesimos.
+// Pode ser mandado uma string 
+const data = new Date('2019-01-20 20:17:00')
+console.log('Dia', data.getDate());
+console.log('Mês', data.getMonth()); // Mês começa do zero
+console.log('Ano', data.getFullYear());
+console.log('Hora', data.getHours());
+console.log('Min', data.getMinutes());
+console.log('Seg', data.getSeconds());
+console.log('ms', data.getMilliseconds());
+console.log('Dia semana', data.getDay()); // 0 é domingo, 6 é sabado
+console.log(data.toString());
+
+function formataData(data) {
+  const dia = zeroAEsquerda(data.getDate());
+  const mes = zeroAEsquerda(data.getMonth() + 1);
+  const ano = zeroAEsquerda(data.getFullYear());
+  const hora = zeroAEsquerda(data.getHours());
+  const seg = zeroAEsquerda(data.getMinutes());
+  const segundos = zeroAEsquerda(data.getSeconds());
+
+  return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`
+}
+
+function zeroAEsquerda (num) {
+  return num >= 10 ? num : `0${num}`
+}
