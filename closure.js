83629722c50e39cef9578c9c1d3a7068f5c3fe67
Canturilsnaik <italo.cantur@gmail.com>
2021-02-06 23:37:06 -0300

    Testando novo aprendizado

---
+++ closure.js
@@ -0,0 +1,25 @@
+// Closure é uma função que consegue ler variaveis dentro e fora do escopo dela mesmo
+
+// Nesse exemplo a função "baa" é uma função closure pois consegue ler a const a que está fora do seu escopo
+
+function foo () {
+  const a = 1 + 2 
+  function baa () {
+    return console.log(a)   
+  }
+
+  baa()
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+arr.forEach(item => {
+  const a = item + 1 
+  function show () {
+    return console.log(a)
+  }
+
+  show()
+})
+
+foo()
