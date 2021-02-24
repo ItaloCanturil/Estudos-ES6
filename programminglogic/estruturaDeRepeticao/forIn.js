b59c7107b9bea0e0cd734405a82ade33aa38d4d0
Canturilsnaik <italo.cantur@gmail.com>
2021-02-23 23:20:25 -0300

    Laço For in

---
+++ programminglogic/estruturaDeRepeticao/forIn.js
@@ -0,0 +1,15 @@
+const frutas = ['Maçã', 'banana', 'Pera'];
+
+for (let i in frutas) {
+  console.log(frutas[i]);
+}
+
+const pessoa = {
+  nome: 'Italo',
+  sobrenome: 'Canturil',
+  idade: 20
+}
+
+for (let key in pessoa) {
+  console.log(pessoa[key])
+}
